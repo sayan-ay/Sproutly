4090b7fe351cd3d45a0380f27fd9c0487b40ca79
Sayan Halder <sayanhalderlaptop08@gmail.com>
2026-04-27 14:43:39 +0530

    Fix errors

--- Client/src/AddPost.jsx
+++ Client/src/AddPost.jsx
@@ -2,19 +2,35 @@ import Editor from "./Editor";
 import { useEffect } from "react";
 import { api } from "./axios";
 import { useEdit } from "./editZustand";
+import toast from "react-hot-toast";
 
 export default function AddPost() {
   const setEdit = useEdit((state) => state.setEdit);
 
   useEffect(() => setEdit(true), []);
 
-  function getText(text) {
-    api
-      .post("/posts/add", {
-        content: text,
-      })
-      .then((res) => alert(res))
-      .catch((err) => alert(err));
+  async function getText(text) {
+
+    try {
+       await toast.promise(
+        api
+          .post("/posts/add", {
+            content: text,
+          }),
+        {
+          loading: 'Saving...',
+          success: <b className="text-green-500">Post added successfully!</b>,
+          error: (err) => <b className="text-red-500">Failed to add post{err.message}</b>,
+        }, {
+          className:"mt-10",
+        }
+      );
+    }
+    
+     
+    catch (err) {
+      console.error("Failed to add post", err);
+      }
   }
 
   return <Editor sendText={getText} />;
